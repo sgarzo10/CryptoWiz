da9e6b140be01b41654140997cfff140787da317
Menoz <mnz3553@gmail.com>
2022-03-05 04:28:23 +0100

    Modifiche Front-End
    Template NFT Info + form mod

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ $(document).ready(() => {
     let menu_list = config.menu;
     col_menu.html(template(menu_list));
     feather.replace();
+    menu_click('HOME')
     return;
 });    
 
@@ -13,7 +14,7 @@ function menu_click(menu_name)
 {
     let col_main = $('#main');
     let template = Handlebars.compile($("#".concat(menu_name.toLowerCase()).concat("-template"))[0].innerHTML);
-    let p_struct={
+    let p_struct = {
         "title": config["page_".concat(menu_name.toLowerCase())]["title"],
         "text": config["page_".concat(menu_name.toLowerCase())]["title"]
     }
@@ -24,9 +25,36 @@ function menu_click(menu_name)
         p_struct["links"] = links;
     }
     col_main.html(template(p_struct));
+    if (p_struct["links"].length > 0) pg_click(0);
     return;
 }
 
+function pg_click(index)
+{
+    let div_pg = $('#pg-t');
+    let template = Handlebars.compile($("#pg-template")[0].innerHTML);
+    let pg_traits = [];
+    //
+    for (let trait of Object.values(nft_list)[index]["traits"])
+    {
+        let pg_text = "";
+        for (let [key, value] of Object.entries(trait))
+        {
+            if ( key === "trait_type") pg_text = value+": ";
+            if ( key === "value") pg_text = pg_text+value;
+        }
+        pg_traits.push(pg_text);
+    }
+    //
+    let p_struct = {
+        "name": Object.values(nft_list)[index]["name"],
+        "image": Object.values(nft_list)[index]["image"],
+        "description": Object.values(nft_list)[index]["description"],
+        "traits": pg_traits
+    };
+    div_pg.html(template(p_struct));
+    return;
+}
 
 
   
